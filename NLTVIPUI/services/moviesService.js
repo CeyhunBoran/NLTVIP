26d880198a30b066e40921816a91d38e50cf3184
Ceyhun Boran <cey.boran@hotmail.com>
2022-08-12 16:42:09 +0300

    Add capital letter for public methods and Add caching.

--- NLTVIPUI/services/moviesService.js
+++ NLTVIPUI/services/moviesService.js
@@ -2,25 +2,32 @@
 app.dataAccess.moviesService = (function () {
     //private
     var result;
+    var moviesCache = new LRUCache(20);
     var loadMovies = function () {
         if (document.getElementById("categories").hasChildNodes()) {
-            let expression = document.getElementsByClassName("active")[0].textContent;
-            let categories = document.getElementsByClassName("category-row");
+            var active = document.getElementsByClassName("active")[0].textContent;
+            var categories = document.getElementsByClassName("category-row");
             for (const element of categories) {
-                app.dataAccess.get({
-                    url: `Content/movies/${expression + "%2F" +element.firstElementChild.textContent}`, onSuccess: function (data) {
-                        result = JSON.parse(data);
-                        app.Components.moviesComponent(result, element);
-                    }, onError: function () { console.log("Request Failed") }
-                });
+                var expression = active + "%2F" + element.firstElementChild.textContent;
+                if (moviesCache.get(expression) != -1) {
+                    var cachedResult = moviesCache.get(expression);
+                    app.Components.moviesComponent(cachedResult, element);
+                }
+                else {
+                    app.dataAccess.get({
+                        url: `Content/movies/${expression}`, onSuccess: function (data) {
+                            result = JSON.parse(data);
+                            moviesCache.put(expression, result);
+                            app.Components.moviesComponent(result, element);
+                        }, onError: function () { console.log("Request Failed") }
+                    });
+                }
             }
-
         };
-
     }
     //public
     return {
-        getMovies: function () {
+        GetMovies: function () {
             loadMovies();
         }
     }
--- NLTVIPUI/services/subCategory.js
+++ NLTVIPUI/services/subCategory.js
@@ -2,21 +2,30 @@
 app.dataAccess.subCategory = (function () {
     //private
     var result;
-    var loadSub = function() {
-        if(document.getElementsByClassName("sidebar-nav")[0].hasChildNodes()){
+    var subCategoryCache = new LRUCache(5);
+    var loadSub = function () {
+        if (document.getElementsByClassName("sidebar-nav")[0].hasChildNodes()) {
             let expression = document.getElementsByClassName("active")[0].textContent;
-            app.dataAccess.get({url:`Content/categories/bymenu/${expression}`, onSuccess: function(data){
-                result = JSON.parse(data);
-                app.Components.subCategoryComponent(result, app.dataAccess.moviesService.getMovies);
-            }, onError: function(){console.log("Request Failed")} });
-            
+            if (subCategoryCache.get(expression) != -1) {
+                var cachedResult = subCategoryCache.get(expression);
+                app.Components.subCategoryComponent(cachedResult, app.dataAccess.moviesService.GetMovies);
+            }
+            else {
+                app.dataAccess.get({
+                    url: `Content/categories/bymenu/${expression}`, onSuccess: function (data) {
+                        result = JSON.parse(data);
+                        subCategoryCache.put(expression, result);
+                        app.Components.subCategoryComponent(result, app.dataAccess.moviesService.GetMovies);
+                    }, onError: function () { console.log("Request Failed") }
+                });
+            }
         }
     }
     //public
-    return{
-        getSubCategories : function() {
+    return {
+        GetSubCategories: function () {
             loadSub();
-        }  
+        }
     }
-    
+
 })();
